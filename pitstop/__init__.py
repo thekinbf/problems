78a976c68bf6fc2fb42ea272549648d02a385ef0
Carter Zenke <5281090+CarterZenke@users.noreply.github.com>
2024-06-25 21:01:24 -0400

    Fix regular expression

--- pitstop/__init__.py
+++ pitstop/__init__.py
@@ -35,21 +35,21 @@ def uses_readline():
 def bahrain():
     """Correctly prints Bahrain data"""
     check_statistics(
-        "bahrain.csv", pitstops=43, fastest=2.23, slowest=52.44, total=202.01
+        "bahrain.csv", pitstops="43", fastest="2.23", slowest="52.44", total="202.01"
     )
 
 
 @check50.check(uses_readline)
 def imola():
     """Correctly prints Imola data"""
-    check_statistics("imola.csv", pitstops=27, fastest=2.09, slowest=10.94, total=83.18)
+    check_statistics("imola.csv", pitstops="27", fastest="2.09", slowest="10.94", total="83.18")
 
 
 @check50.check(uses_readline)
 def jeddah():
     """Correctly prints Jeddah data"""
     check_statistics(
-        "jeddah.csv", pitstops=19, fastest=2.44, slowest=41.61, total=131.53
+        "jeddah.csv", pitstops="19", fastest="2.44", slowest="41.61", total="131.53"
     )
 
 
@@ -57,21 +57,21 @@ def jeddah():
 def melbourne():
     """Correctly prints Melbourne data"""
     check_statistics(
-        "melbourne.csv", pitstops=36, fastest=2.1, slowest=31.18, total=159.11
+        "melbourne.csv", pitstops="36", fastest="2.1", slowest="31.18", total="159.11"
     )
 
 
 @check50.check(uses_readline)
 def miami():
     """Correctly prints Miami data"""
-    check_statistics("miami.csv", pitstops=28, fastest=1.94, slowest=11.05, total=98.53)
+    check_statistics("miami.csv", pitstops="28", fastest="1.94", slowest="11.05", total="98.53")
 
 
 @check50.check(uses_readline)
 def shanghai():
     """Correctly prints Shanghai data"""
     check_statistics(
-        "shanghai.csv", pitstops=39, fastest=1.9, slowest=19.35, total=135.82
+        "shanghai.csv", pitstops="39", fastest="1.9", slowest="19.35", total="135.82"
     )
 
 
@@ -79,25 +79,25 @@ def shanghai():
 def suzuka():
     """Correctly prints Suzuka data"""
     check_statistics(
-        "suzuka.csv", pitstops=36, fastest=2.08, slowest=5.43, total=105.32
+        "suzuka.csv", pitstops="36", fastest="2.08", slowest="5.43", total="105.32"
     )
 
 
 def check_statistics(
-    filename: str, pitstops: int, fastest: int, slowest: int, total: int
+    filename: str, pitstops: str, fastest: str, slowest: str, total: str
 ):
     out = check50.run(f"Rscript pitstop.R {filename}").stdout()
 
-    if not re.search(rf"\D{pitstops}\D", out):
+    if not re.search(rf"\b{re.escape(pitstops)}\b", out):
         raise check50.Failure(f"Could not find {pitstops} pitstops in output.")
 
-    if not re.search(rf"\D{fastest}\D", out):
+    if not re.search(rf"\b{re.escape(fastest)}\b", out):
         raise check50.Failure(f"Could not find fastest time of {fastest}s in output.")
 
-    if not re.search(rf"\D{slowest}\D", out):
+    if not re.search(rf"\b{re.escape(slowest)}\b", out):
         raise check50.Failure(f"Could not find slowest time of {slowest}s in output.")
 
-    if not re.search(rf"\D{total}\D", out):
+    if not re.search(rf"\b{re.escape(total)}\b", out):
         raise check50.Failure(f"Could not find total time of {total}s in output.")
 
 
