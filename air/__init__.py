9559a781707f9ad40daba3218b9c5d623869ccf8
Carter Zenke <5281090+CarterZenke@users.noreply.github.com>
2024-06-29 15:20:18 -0400

    Move `.include` to individual checks

--- air/__init__.py
+++ air/__init__.py
@@ -5,16 +5,12 @@ def exists():
     """.RData files exist"""
     for filename in ["air.RData", "2.RData", "3.RData", "4.RData", "5.RData", "6.RData", "7.RData"]:
         check50.exists(filename)
-    
-    check50.include("check_files/check_air.RData")
-    check50.include("check_files/check_5.RData")
-    check50.include("check_files/check_6.RData")
-    check50.include("check_files/check_7.RData")
 
 
 @check50.check(exists)
 def check_1():
     """air.RData contains correctly cleaned data"""
+    check50.include("check_files/check_air.RData")
     run_check("check_1.R")
 
 
@@ -39,18 +35,21 @@ def check_4():
 @check50.check(check_4)
 def check_5():
     """5.RData contains air tibble with largest pollutant source for each county"""
+    check50.include("check_files/check_5.RData")
     run_check("check_5.R")
 
 
 @check50.check(check_5)
 def check_6():
     """6.RData contains air tibble with total emissions for each pollutant"""
+    check50.include("check_files/check_6.RData")
     run_check("check_6.R")
 
 
 @check50.check(check_6)
 def check_7():
     """7.RData contains air tibble with total emissions of each pollutant for each level 1 source"""
+    check50.include("check_files/check_7.RData")
     run_check("check_7.R")
 
 
