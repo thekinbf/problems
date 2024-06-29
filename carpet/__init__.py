a91e86b0f085445beb81eed19a656161c19b8cab
Carter Zenke <5281090+CarterZenke@users.noreply.github.com>
2024-06-29 12:25:35 -0400

    Add status check to carpet

--- carpet/__init__.py
+++ carpet/__init__.py
@@ -26,19 +26,35 @@ def readline():
 @check50.check(readline)
 def twenty_four():
     """carpet.R correctly predicts 2024 visitors"""
-    check50.run("Rscript carpet.R 2024").stdout("10.24 million visitors")
+    check_prediction("2024", "10.24 million visitors")
 
 
 @check50.check(readline)
 def thirty_four():
     """carpet.R correctly predicts 2034 visitors"""
-    check50.run("Rscript carpet.R 2034").stdout("12.14 million visitors")
+    check_prediction("2034", "12.14 million visitors")
 
 
 @check50.check(readline)
 def fourteen():
     """carpet.R correctly predicts 2014 visitors"""
-    check50.run("Rscript carpet.R 2014").stdout("8.34 million visitors")
+    check_prediction("2014", "8.34 million visitors")
+
+
+def check_prediction(year: str, output: str) -> None:
+    status = check50.run(f"Rscript carpet.R {year}").exit()
+    out = check50.run(f"Rscript carpet.R {year}").stdout()
+
+    if status != 0:
+        if match := re.search(r"cannot open file '(?P<filename>[^']+)'", out):
+            raise check50.Failure(
+                f'carpet.R could not open "{match.group("filename")}"',
+                help='Be sure to provide a relative path, such as "visitors.csv"',
+            )
+        raise check50.Failure(f"carpet.R encountered an error: {out}")
+
+    if output not in out:
+        raise check50.Failure(f'Could not find "{output}" in carpet.R\'s output')
 
 
 def replace_readline(contents: list[str]) -> list[str]:
