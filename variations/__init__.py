5d5ff6295b4965801ffd15a1588525e2285b0333
Carter Zenke <5281090+CarterZenke@users.noreply.github.com>
2024-06-29 13:16:25 -0400

    Add status check to variations

--- variations/__init__.py
+++ variations/__init__.py
@@ -10,7 +10,11 @@ def exists():
 @check50.check(exists)
 def variations():
     """variations.R prints random sequences of 20 characters"""
+    status = check50.run("Rscript variations.R").exit(timeout=6)
     out = check50.run("Rscript variations.R").stdout(timeout=6).strip()
-    length = len(out)
-    if length != 20:
+    
+    if status != 0:
+        raise check50.Failure(out)
+    
+    if (length := len(out)) != 20:
         raise check50.Failure(f"variations.R printed a sequence of {length} characters")
