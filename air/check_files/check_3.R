cc1a5e3ccabf6f3b2d077dce7b0adc463af07ca1
Carter Zenke <5281090+CarterZenke@users.noreply.github.com>
2024-06-29 15:31:09 -0400

    Add prechecks to `3.R` check

--- air/check_files/check_3.R
+++ air/check_files/check_3.R
@@ -1,4 +1,33 @@
-load("3.RData")
+load_successful <- function(filename) {
+  tryCatch({
+    load(filename, envir = .GlobalEnv)
+    TRUE
+  }, error = function(e) {
+    FALSE
+  }, warning = function(w) {
+    FALSE
+  }, message = function(m) {
+    FALSE
+  })
+}
+
+# Check if load was successful
+if (!load_successful("3.RData")) {
+  cat("Failed to load 3.RData")
+  quit(status = 1)
+}
+
+# Check if an air object exists
+if (!exists("air")) {
+  cat("`air` tibble does not exist in 3.RData")
+  quit(status = 1)
+}
+
+# Check if air is a tibble
+if (!("tbl_df" %in% class(air))) {
+  cat("`air` in 3.RData is not a tibble")
+  quit(status = 1)
+}
 
 # Check if the county vector has only a single unique county
 if (length(unique(air$county)) != 1) {
