40fab07f06614d92cc913fd5cd78755f9a40ac44
Carter Zenke <5281090+CarterZenke@users.noreply.github.com>
2024-07-06 17:54:50 -0400

    Do not check attributes or names

--- air/check_files/check_6.R
+++ air/check_files/check_6.R
@@ -62,7 +62,7 @@ if (length(missing_columns) > 0) {
 }
 
 # Objects are equal
-if (!isTRUE(all.equal(air, check_air))) {
+if (!isTRUE(all.equal(air, check_air, check.attributes = FALSE, check.names = FALSE))) {
   cat("tibble in 6.RData contains rows that are out of order or different from what's expected")
   quit(status = 1)
 }
