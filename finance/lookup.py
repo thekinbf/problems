30c6ca19b4528e80d2fa133484d40fe24b5ec0d7
Brenda Anderson <curiouskiwi@users.noreply.github.com>
2024-09-11 10:28:39 +1200

    Update lookup.py
    typo

--- finance/lookup.py
+++ finance/lookup.py
@@ -5,6 +5,6 @@ def lookup(symbol):
     elif (symbol == "BBBB"):
         return {"name": "BBBB stock", "price": 14.00, "symbol": "BBBB"}
     elif (symbol == "CCCC"):
-        return {"name": CCCC stock", "price": 2000.00, "symbol": "CCCC"}
+        return {"name": "CCCC stock", "price": 2000.00, "symbol": "CCCC"}
     else:
         return None
