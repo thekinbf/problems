dfd0710f143b0f8fd2f9f7845759bb71c672e597
Brenda Anderson <curiouskiwi@users.noreply.github.com>
2024-09-11 09:59:51 +1200

    Update lookup.py
    add the name to the lookup return values (our new in-house api returns the name)

--- finance/lookup.py
+++ finance/lookup.py
@@ -1,10 +1,10 @@
 def lookup(symbol):
     symbol = symbol.upper()
     if (symbol == "AAAA"):
-        return {"price": 28.00, "symbol": "AAAA"}
+        return {"name": "AAAA stock", "price": 28.00, "symbol": "AAAA"}
     elif (symbol == "BBBB"):
-        return {"price": 14.00, "symbol": "BBBB"}
+        return {"name": "BBBB stock", "price": 14.00, "symbol": "BBBB"}
     elif (symbol == "CCCC"):
-        return {"price": 2000.00, "symbol": "CCCC"}
+        return {"name": CCCC stock", "price": 2000.00, "symbol": "CCCC"}
     else:
         return None
